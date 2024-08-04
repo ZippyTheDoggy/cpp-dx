5837b6763e8cf748f7cc0d97de6eea69ba48f20d
ZippyTheDoggy <ZippyTheDoggyYT@gmail.com>
2024-08-03 22:23:05 -0500

    initial upload

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	g++ main.cpp -o main.exe -std=c++20 -Wall -static-libstdc++
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    int a = 5;
+    int b = 10;
+    std::cout << a + b << std::endl;
+
+    return EXIT_SUCCESS;
+}
